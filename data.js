ef1de68484eee3375e401344e28c8dafe7c02176
jcerveny96 <jan.cerveny.1996@gmail.com>
2025-12-23 21:22:46 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,21 +1,25 @@
 const shipments = {
   DARKY123: {
-    name: "Krabice plná vánoční radosti 🎁",
-    sentFrom: "Brno",
-    sentAt: "15. 12. 2025 14:32",
-    lastSeen: "Praha – třídicí centrum",
-    lastSeenAt: "18. 12. 2025 09:10",
-    eta: "24. 12. 2025",
+    name: "Krabice plná tekuté radosti, 5 L 🎁",
+    sentFrom: "Tsivaras Apokoronou, Chania, Kréta",
+    sentAt: "21. 10. 2025 14:32",
+    lastSeen: "Depo Bratislava – třídicí centrum",
+    lastSeenAt: "30. 11. 2025 09:10",
+    lastSeenAfter: "Depo Brusel 2 - rozvozové centrum RADO",
+    lastSeenAfterAt: "25. 11. 2025 16:49",
+    eta: "31. 12. 2025 v odpoledních hodinách",
     lat: 50.0755,
     lng: 14.4378
   },
   LOL456: {
-    name: "Extrémně tajná zásilka",
-    sentFrom: "Ostrava",
-    sentAt: "16. 12. 2025 08:01",
+    name: "Mastná, tekutá radost, 2.5 L 🎁",
+    sentFrom: "Tsivaras Apokoronou, Chania, Kréta",
+    sentAt: "21. 10. 2025 14:32",
     lastSeen: "Neznámá lokace",
     lastSeenAt: "???",
-    eta: "možná někdy",
+    lastSeenAfter: "Depo Brusel 2 - rozvozové centrum RADO",
+    lastSeenAfterAt: "25. 11. 2025 16:49",
+    eta: "po domluvě s příjemcem - nejdříve 2. 1. 2026",
     lat: 49.8209,
     lng: 18.2625
   }
