8c6131af3ff4e5dd9c177f4abc167d84d6451f25
jcerveny96 <jan.cerveny.1996@gmail.com>
2025-12-23 19:07:21 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,22 @@
-
+const shipments = {
+  DARKY123: {
+    name: "Krabice plná vánoční radosti 🎁",
+    sentFrom: "Brno",
+    sentAt: "15. 12. 2025 14:32",
+    lastSeen: "Praha – třídicí centrum",
+    lastSeenAt: "18. 12. 2025 09:10",
+    eta: "24. 12. 2025",
+    lat: 50.0755,
+    lng: 14.4378
+  },
+  LOL456: {
+    name: "Extrémně tajná zásilka",
+    sentFrom: "Ostrava",
+    sentAt: "16. 12. 2025 08:01",
+    lastSeen: "Neznámá lokace",
+    lastSeenAt: "???",
+    eta: "možná někdy",
+    lat: 49.8209,
+    lng: 18.2625
+  }
+};
